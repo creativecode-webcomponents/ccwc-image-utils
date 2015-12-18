ce7a93436266ed6a03b2002315b9768a6ab92ac1
bengfarrell <ben@benfarrell.com>
2015-12-17 20:42:14 -0800

    movin files around

--- gulpfile.js
+++ gulpfile.js
@@ -4,5 +4,5 @@ var gl2js = require('gulp-gl2js');
 gulp.task('default', function(cb) {
     gulp.src('./src/shaders/*.glsl')
         .pipe(gl2js('shaders', { assignto: 'ccwc.WebGLFilter.shaders'} ))
-        .pipe(gulp.dest('build'));
+        .pipe(gulp.dest('./src'));
 });
