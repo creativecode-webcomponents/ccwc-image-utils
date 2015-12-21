09414cd4fe7d7f382ac9d64d89eeebec50d66d2d
bengfarrell <ben@benfarrell.com>
2015-12-20 20:44:35 -0800

    added a set of extra non-standard shaders to play around with

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,23 @@
 var gulp = require('gulp');
 var gl2js = require('gulp-gl2js');
 
-gulp.task('default', function(cb) {
+/**
+ * Regular Shaders (a small set to stay minimal)
+ */
+gulp.task('shaders', function(cb) {
     gulp.src('./src/shaders/*.glsl')
         .pipe(gl2js('shaders', { assignto: 'ccwc.WebGLFilter.shaders'} ))
         .pipe(gulp.dest('./src'));
 });
+
+/**
+ * Extra shaders, more of a playground to test a bunch of them
+ */
+gulp.task('extra-shaders', function(cb) {
+    gulp.src('./src/extra-shaders/*.glsl')
+        .pipe(gl2js('extra-shaders', { assignto: 'var filters'} ))
+        .pipe(gulp.dest('./src'));
+});
+
+
+gulp.task('default', ['shaders', 'extra-shaders']);
